57a7a2ad76fab91c4b16b9493bdf4e2f118a89fd
agus-enc <aencalada@fi.uba.ar>
2026-05-17 22:50:06 -0300

    Simplifique validar_id_usuario

--- icebox/validators/usuarios.py
+++ icebox/validators/usuarios.py
@@ -1,5 +1,4 @@
 from ..utils import construir_error, validar_entero, validar_minimo
 
-def validar_id_usuario(id_usuario: str) -> int:
-    id_usuario = validar_entero(id_usuario)
+def validar_id_usuario(id_usuario: int) -> int:
     return validar_minimo(id_usuario, 0, 'id')
