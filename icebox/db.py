8470b92b09d9d0c504768197a9a84298af67ccb9
agus-enc <aencalada@fi.uba.ar>
2026-05-17 22:50:47 -0300

    Implemente funciones para ejecutar consultas y obtener viajes a db.py

--- icebox/db.py
+++ icebox/db.py
@@ -1,5 +1,4 @@
 from mysql.connector import connect
-from utils import construir_error
 
 def get_connection():
     return connect(
@@ -9,7 +8,27 @@ def get_connection():
         database="ICEBOX"
     )
 
-def ejecutar_mutacion(sql: str, parametros: dict):
+def ejecutar_consulta(sql: str, parametros: dict) -> list:
+    """
+        Ejecuta un SELECT y devuelve todas las filas como lista de dicts.
+    """
+    conn = None
+    cursor = None
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute(sql, parametros or {})
+        resultados = cursor.fetchall()
+        return resultados
+
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+def ejecutar_mutacion(sql: str, parametros: dict) -> int:
     """
         Ejecuta un INSERT, UPDATE o DELETE y hace commit.
         Retorna el id generado en caso de INSERT, o 0 en otro caso.
@@ -20,14 +39,10 @@ def ejecutar_mutacion(sql: str, parametros: dict):
         conn = get_connection()
         cursor = conn.cursor(dictionary=True)
 
-        resultado = cursor.execute(sql, parametros or {})
-
+        cursor.execute(sql, parametros or {})
         conn.commit()
 
         return cursor.lastrowid or 0
-
-    except Exception as error:
-        raise ValueError(construir_error(500, "Internal Server Error", "Problema inesperado en el servidor"))
 
     finally:
         if cursor:
@@ -35,9 +50,17 @@ def ejecutar_mutacion(sql: str, parametros: dict):
         if conn:
             conn.close()
 
-def insertar_viaje(id_usuario, titulo, fecha):
-    """Inserta un nuevo usuario y retorna el id generado."""
+def insertar_viaje(id_usuario: int, titulo: str, fecha_viaje: str) -> int:
+    """Inserta un nuevo viaje y retorna el id generado."""
 
     sql = 'INSERT INTO viajes (id_usuario, titulo, fecha_viaje) VALUES (%(id_usuario)s, %(titulo)s, %(fecha_viaje)s)'
 
-    return ejecutar_mutacion(sql, {"id_usuario": id_usuario, "titulo": titulo, "fecha_viaje": fecha})
+    return ejecutar_mutacion(sql, {"id_usuario": id_usuario, "titulo": titulo, "fecha_viaje": fecha_viaje})
+
+def obtener_viaje(id_viaje: int) -> dict | None:
+    """Obtener un viaje especifico por id."""
+
+    sql = 'SELECT * FROM viajes WHERE id_viaje = %(id_viaje)s'
+    resultados = ejecutar_consulta(sql, {"id_viaje": id_viaje})
+
+    return resultados[0] if resultados else None
