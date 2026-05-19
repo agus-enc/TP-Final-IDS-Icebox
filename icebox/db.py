a94b30559b25ca9167c479a9f0527aef4ccc1413
Lujan_Bajinay <lbajinay@fi.uba.ar>
2026-05-19 17:48:27 -0300

    Implementacion de funciones necesarias, para usuarios, para utilizar en los endpoints de usuarios

--- icebox/db.py
+++ icebox/db.py
@@ -124,3 +124,42 @@ def actualizar_posicion_iman(id_iman: int, ubicacion_heladera: bool, posicion_x:
         "posicion_y": posicion_y
     })
     return filas_afectadas > 0
+
+def insertar_usuario(nombre_usuario: str, email: str, password: str)-> int:
+    """Inserta un nuevo usuario y devuelve el id generado"""
+    sql = """
+        INSERT INTO usuarios (nombre_usuario, email, password)
+        VALUES (%(nombre_usuario)s, %(email)s, %(password)s)
+        """
+        
+    return ejecutar_mutacion(sql, {"nombre_usuario": nombre_usuario, "email": email, "password": password})
+
+def obtener_usuario_por_email(email: str)-> dict | None:
+    """Busca un usuario por su mail (util para el login)"""
+    sql = "SELECT * FROM usuarios WHERE email = %(email)s"
+    resultados = ejecutar_consulta(sql, {"email": email})
+    return resultados[0] if resultados else None
+
+def actualizar_nombre_usuario(id_usuario: int, nombre_usuario: str)-> bool:
+    """Actualiza el nombre de usuario"""
+    sql = "UPDATE usuarios SET nombre_usuario = %(nombre_usuario)s WHERE id_usuario = %(id_usuario)s"
+    filas_afectadas = ejecutar_mutacion(sql, {"id_usuario": id_usuario, "nombre_usuario": nombre_usuario})
+
+    return filas_afectadas > 0
+
+def actualizar_mail_usuario(id_usuario: int, email: str)-> bool:
+    """Actualiza el correo electronico"""
+    sql = "UPDATE usuarios SET email = %(email)s WHERE id_usuario = %(id_usuario)s"
+    filas_afectadas = ejecutar_mutacion(sql, {"id_usuario": id_usuario, "email": email})
+
+    return filas_afectadas > 0
+
+def actualizar_password_usuario(id_usuario: int, password: str)-> bool:
+    """Actualiza la contraseña del usuario"""
+    sql = "UPDATE usuarios SET password = %(password)s WHERE id_usuario = %(id_usuario)s"
+    filas_afectadas = ejecutar_mutacion(sql, {"id_usuario": id_usuario, "password": password})
+
+    return filas_afectadas > 0
+
+
+ 
