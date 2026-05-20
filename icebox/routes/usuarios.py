ad3ca614deb8800f67ec6c9d488272bd51065259
Lujan_Bajinay <lbajinay@fi.uba.ar>
2026-05-19 23:15:19 -0300

    Implementacion del  endpoint para registrar un usuario

--- icebox/routes/usuarios.py
+++ icebox/routes/usuarios.py
@@ -1,9 +1,29 @@
 from flask import Blueprint, jsonify, request
 from ..utils import construir_error
-from ..validators.usuarios import validar_id_usuario
-from ..services.usuarios import eliminar_usuario
+from ..validators.usuarios import validar_id_usuario, validar_registro_usuario, validar_login_usuario
+from ..services.usuarios import eliminar_usuario, registrar_nuevo_usuario, autenticar_usuario, obtener_perfil_usuario, modificar_nombre, modificar_email, modificar_password
 
 usuarios_bp = Blueprint("usuarios", __name__)
+
+@usuarios_bp.route("/usuarios", methods=['POST'])
+def registrar_usuario():
+    data = request.get_json() or {}
+    nombre_usuario = data.get('nombre_usuario')
+    email = data.get("email")
+    password = data.get("password")
+
+    if not nombre_usuario or not email or not password:
+        error_body = construir_error('missing_fields', 'Campos faltantes', 'nombre_usuario, email y password son requeridos para crear una cuenta')
+        return jsonify(error_body), 400
+    
+    try:
+        nuevo_id = registrar_nuevo_usuario(nombre_usuario, email, password)
+        return jsonify({"message": "Usuario registrado con exito", "id_usuario": nuevo_id}), 201
+    
+    except Exception as e:
+        error_body = construir_error('database.error', 'Error al registrar', str(e))
+        return jsonify(error_body), 400
+
 
 @usuarios_bp.route("/usuarios/<int:id_usuario>", methods=["DELETE"])
 def delete_usuario(id_usuario):
