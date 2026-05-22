a7acbdaecfd3bd0193d93305fe9199e7ee850cd2
victoria <vstefanutti@fi.uba.ar>
2026-05-22 19:11:16 -0300

    implementacion de endpoint para obtener paises

--- backend/endpoints/routes/lugares.py
+++ backend/endpoints/routes/lugares.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
-from ..services.lugares import obtener_paises_visitados
+from ..services.lugares import obtener_paises_visitados, obtener_paises
+from ..utils import construir_error
 
 lugares_bp = Blueprint("lugares", __name__)
 
@@ -12,3 +13,15 @@ def get_paises_visitados(id_usuario):
         error_dict = e.args[0]
         status = e.args[1] if len(e.args) > 1 else 400
         return jsonify(error_dict), status
+
+@lugares_bp.route("/paises", methods=["GET"])
+def get_paises():
+    try:
+        paises_dto = obtener_paises()
+        return jsonify(paises_dto), 200
+    except Exception:
+        return jsonify(construir_error(
+            code="SERVER_ERROR",
+            message="No se pudieron obtener los países",
+            description="Ocurrio error interno al procesar la solicitud"
+        )), 500
