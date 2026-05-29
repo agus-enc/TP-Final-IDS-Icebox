0426f4f528bc077d7a82b7c35b0c9d8e9ab2a44c
AilenLopez <avlopez@fi.uba.ar>
2026-05-29 14:55:27 -0300

    Cambio de Mapa

--- frontend/static/js/map.js
+++ frontend/static/js/map.js
@@ -1,256 +1,72 @@
-import * as THREE from "three";
-import {OrbitControls} from "three/addons/controls/OrbitControls.js";
-import {GUI} from "https://esm.sh/lil-gui@0.17.0";
+const map = L.map('map', { // Limites del mapa 
+  minZoom: 2,
+  maxBounds: [
+    [-90, -180], 
+    [90, 180]
+  ],
+  maxBoundsViscosity: 1.0
+}).setView([27, 0], 2);
 
-const containerEl = document.querySelector(".globe-wrapper");
-const canvasEl = containerEl.querySelector("#globe-3d");
-const svgMapDomEl = document.querySelector("#map");
-const svgCountries = Array.from(svgMapDomEl.querySelectorAll("path"));
-const countryNameEl = document.querySelector(".info span");
+// Tiles (OpenStreetMap)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  noWrap: true,
+  attribution: '&copy; OpenStreetMap'
+}).addTo(map);
 
-let renderer, scene, camera, rayCaster, pointer, controls;
-let globeGroup, globeColorMesh, globeStrokesMesh;
+var geojson;
 
-const svgViewBox = [2000, 1000];
-const offsetY = -.1;
-
-const params = {
-    strokeColor: "#111111",
-    defaultColor: "#9a9591",
-    visitedColor: "#00C9A2", // Color predeterminado para los países visitados
-    fogColor: "#e4e5e6",
-    fogDistance: 2.6,
-    strokeWidth: 2,
-    hiResScalingFactor: 2,
-}
-
-
-let hoveredCountryIdx = 0;
-let isTouchScreen = false;
-let isHoverable = true;
-
-const textureLoader = new THREE.TextureLoader();
-let staticMapUri;
-const bBoxes = [];
-
-const paisesVisitados = ["China","Argentina"] // Simulación de Base de Datos
-
-initScene();
-createControls();
-
-window.addEventListener("resize", updateSize);
-
-containerEl.addEventListener("touchstart", (e) => {
-    isTouchScreen = true;
-});
-containerEl.addEventListener("mousemove", (e) => {
-    updateMousePosition(e.clientX, e.clientY);
-});
-containerEl.addEventListener("click", (e) => {
-    updateMousePosition(e.clientX, e.clientY);
-});
-
-function updateMousePosition(eX, eY) {
-    pointer.x = (eX - containerEl.offsetLeft) / containerEl.offsetWidth * 2 - 1;
-    pointer.y = -((eY - containerEl.offsetTop) / containerEl.offsetHeight) * 2 + 1;
-}
-
-function initScene() {
-    renderer = new THREE.WebGLRenderer({canvas: canvasEl, alpha: true});
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-    scene = new THREE.Scene();
-    scene.fog = new THREE.Fog(params.fogColor, 0, params.fogDistance);
-
-    camera = new THREE.OrthographicCamera(-1.2, 1.2, 1.2, -1.2, 0, 3);
-    camera.position.z = 1.3;
-
-    globeGroup = new THREE.Group();
-    scene.add(globeGroup);
-
-    rayCaster = new THREE.Raycaster();
-    rayCaster.far = 1.15;
-    pointer = new THREE.Vector2(-1, -1);
-
-    createOrbitControls();
-    createGlobe();
-    prepareHiResTextures();
-
-    updateSize();
-    gsap.ticker.add(render);
-}
-
-function createOrbitControls() {
-    controls = new OrbitControls(camera, canvasEl);
-    controls.enablePan = false;
-    controls.enableZoom = false;
-    controls.enableDamping = true;
-    controls.minPolarAngle = .46 * Math.PI;
-    controls.maxPolarAngle = .46 * Math.PI;
-    controls.autoRotate = true;
-    controls.autoRotateSpeed *= 1.2;
-
-    controls.addEventListener("start", () => {
-        isHoverable = false;
-        pointer = new THREE.Vector2(-1, -1);
-        gsap.to(globeGroup.scale, {
-            duration: .3,
-            x: .9,
-            y: .9,
-            z: .9,
-            ease: "power1.inOut"
-        })
+function highlightFeature(e) { // Resaltar el país al pasar el mouse
+    var layer = e.target;
+    layer.setStyle({
+        weight: 2,
+        color: '#696969ac',
+        dashArray: '',
+        fillOpacity: 0.7
     });
-    controls.addEventListener("end", () => {
-        // isHoverable = true;
-        gsap.to(globeGroup.scale, {
-            duration: .6,
-            x: 1,
-            y: 1,
-            z: 1,
-            ease: "back(1.7).out",
-            onComplete: () => {
-                isHoverable = true;
-            }
-        })
-    });
+    info.update(layer.feature.properties);
+}
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
 }
 
-function createGlobe() {
-    const globeGeometry = new THREE.IcosahedronGeometry(1, 20);
+const urlPaises = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json';
 
-    const globeColorMaterial = new THREE.MeshBasicMaterial({
-        transparent: true,
-        alphaTest: true,
-        side: THREE.DoubleSide
-    });
-    const globeStrokeMaterial = new THREE.MeshBasicMaterial({
-        transparent: true,
-        depthTest: false,
-    });
+fetch(urlPaises)
+  .then(response => response.json())
+  .then(data => {
+    geojson = L.geoJson(data, {
+      style: function(feature) {
+        return {
+          color: "#2b2b2b29", // Color de la frontera
+          weight: 1.5,
+          fillColor: "#2b81c800", // Color del país
+          fillOpacity: 0.1
+        };
+      },
+      onEachFeature: function(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight
+        });
+      }
+    }).addTo(map);
+  })
+  .catch(function(error) {
+    console.error("Error al cargar las divisiones del mundo:", error);
+  });
 
-    globeColorMesh = new THREE.Mesh(globeGeometry, globeColorMaterial);
-    globeStrokesMesh = new THREE.Mesh(globeGeometry, globeStrokeMaterial);
-    
-    globeStrokesMesh.renderOrder = 2;
-    globeGroup.add(globeStrokesMesh, globeColorMesh);
-}
+var info = L.control();
 
-function setMapTexture(material, URI) {
-    textureLoader.load(URI, (t) => {
-        t.repeat.set(1, 1);
-        material.map = t;
-        material.needsUpdate = true;
-    });
-}
-
-function prepareHiResTextures() {
-    let svgData;
-    // Se colorean todos los países
-    svgCountries.forEach((path, idx) => {
-        const countryName = path.getAttribute("data-name");
-        bBoxes[idx] = path.getBBox();
-        // Se colorean los paises visitados de celeste y los no visitados de gris dependiendo de su estado en la tabla 
-        if (paisesVisitados.includes(countryName)) {
-            path.setAttribute("fill", params.visitedColor);
-        } else {
-            path.setAttribute("fill", params.defaultColor);
-        }
-        // Pinta el fondo y el borde al mismo tiempo (evitar problemas de aliasing)
-        path.setAttribute("stroke", params.strokeColor);
-        path.setAttribute("stroke-width", params.strokeWidth);
-    });
-    
-    gsap.set(svgMapDomEl, {
-        attr: {
-            "viewBox": "0 " + (offsetY * svgViewBox[1]) + " " + svgViewBox[0] + " " + svgViewBox[1],
-            "width": svgViewBox[0] * params.hiResScalingFactor,
-            "height": svgViewBox[1] * params.hiResScalingFactor,
-        }
-    });
-
-    svgData = new XMLSerializer().serializeToString(svgMapDomEl);
-    staticMapUri = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(svgData);
-    
-    setMapTexture(globeColorMesh.material, staticMapUri);
-    setMapTexture(globeStrokesMesh.material, staticMapUri);
-    
-    if (svgCountries[hoveredCountryIdx]) {
-        countryNameEl.innerHTML = svgCountries[hoveredCountryIdx].getAttribute("data-name");
-    }
-}
-
-function updateMap(uv = {x: 0, y: 0}) {
-    const pointObj = svgMapDomEl.createSVGPoint();
-    pointObj.x = uv.x * svgViewBox[0];
-    pointObj.y = (1 + offsetY - uv.y) * svgViewBox[1];
-
-    for (let i = 0; i < svgCountries.length; i++) {
-        const boundingBox = bBoxes[i];
-        if (
-            pointObj.x > boundingBox.x && //AND
-            pointObj.x < boundingBox.x + boundingBox.width &&
-            pointObj.y > boundingBox.y &&
-            pointObj.y < boundingBox.y + boundingBox.height
-        ) {
-            const isHovering = svgCountries[i].isPointInFill(pointObj);
-            if (isHovering) {
-                if (i !== hoveredCountryIdx) {
-                    hoveredCountryIdx = i;
-                    // solo cambia el texto 
-                    countryNameEl.innerHTML = svgCountries[hoveredCountryIdx].getAttribute("data-name");
-                    break;
-                }
-            }
-        }
-    }
-}
-
-function render() {
-    controls.update();
-
-    if (isHoverable) {
-        rayCaster.setFromCamera(pointer, camera);
-        const intersects = rayCaster.intersectObject(globeStrokesMesh);
-        if (intersects.length) {
-            updateMap(intersects[0].uv);
-        }
-    }
-
-    if (isTouchScreen && isHoverable) {
-        isHoverable = false;
-    }
-
-    renderer.render(scene, camera);
-}
-
-function updateSize() {
-    // Tamaño del canvas para que ocupe el máximo espacio de la pantalla
-    const availableHeight = window.innerHeight;
-    const availableWidth = window.innerWidth;
-    const side = Math.min(availableWidth, availableHeight);
-    
-    containerEl.style.width = side + "px";
-    containerEl.style.height = side + "px";
-    if (renderer) {
-        renderer.setSize(side, side);
-    }
-    if (camera) {
-        camera.updateProjectionMatrix();
-    }
-}
-
-function createControls() {
-    const gui = new GUI();
-    gui.close();
-    
-    gui.addColor(params, "strokeColor").onChange(prepareHiResTextures).name("stroke");
-    gui.addColor(params, "defaultColor").onChange(prepareHiResTextures).name("gris (no visitado)");
-    gui.addColor(params, "visitedColor").onChange(prepareHiResTextures).name("celeste (visitado)");
-    gui.addColor(params, "fogColor").onChange(() => {
-        scene.fog = new THREE.Fog(params.fogColor, 0, params.fogDistance);
-    }).name("fog");
-    gui.add(params, "fogDistance", 1, 4).onChange(() => {
-        scene.fog = new THREE.Fog(params.fogColor, 0, params.fogDistance);
-    }).name("fog distance");
-}
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); 
+    this.update();
+    return this._div;
+};
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Información del País</h4>' +  (props ?
+        '<b>' + props.name + '</b>'
+        : 'Pasa el cursor sobre un país');
+};
+info.addTo(map);
