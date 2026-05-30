81b00b2f1476d48a4fc72b32d2550814be647a19
Lujan_Bajinay <lbajinay@fi.uba.ar>
2026-05-30 19:11:41 -0300

    implementacion del archivo admin.py para la creacion del grafico y de reportes

--- backend/endpoints/services/admin.py
+++ backend/endpoints/services/admin.py
@@ -3,4 +3,71 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from reportlab.pdfgen import canvas
+from db import ejecutar_consulta
 
+def obtener_estadisticas_viajes():
+    """ Hace la consulta a la base de datos usando db.py para traer los destinos y cuantos viajes tiene cada uno """
+
+    sql = """
+        SELECT ciudades.nombre, COUNT(paradas.id_parada) AS cantidad
+        FROM paradas
+        INNER JOIN ciudades ON paradas.id_parada = ciudades.id_ciudad
+        GROUP BY ciudades.id_ciudad, ciudades.nombre
+        ORDER BY cantidad DESC
+        LIMIT 5
+    """
+    resultados = ejecutar_consulta(sql, None)
+    return resultados
+
+
+def generar_grafico_viajes():
+
+    datos_db = obtener_estadisticas_viajes()
+    ciudades = []
+    cantidades = []
+
+    if not datos_db:
+        ciudades = ['Sin viajes cargados']
+        cantidades = [0]
+    else:
+        for fila in datos_db:
+            ciudades.append(fila['nombre'])
+            cantidades.append(fila['cantidad'])
+    
+    plt.figure(figsize=(6,4))
+    plt.bar(ciudades, cantidades, color='#7485e6', edgecolor='#333333', linewidth=2)
+
+    plt.title('Ciudades Mas visitadas (Top 5)', fontsize=12, fontweight='bold', pad=15)
+    plt.xlabel('Ciudades', fontweight='bold')
+    plt.ylabel('Cantidad de Visitas', fontweight='bold')
+
+    ruta_grafico = os.path.join('frontend', 'static', 'images', 'grafico_admin.png')
+    os.makedirs(os.path.dirname(ruta_grafico), exist_ok=True)
+
+    plt.savefig(ruta_grafico, bbox_inches='tight', dpi=1000)
+    plt.close()
+    return ruta_grafico
+
+def generar_pdf_reporte(ruta_grafico):
+    """
+    Genera el reporte PDF insertando la imagen del grafico
+    """
+    ruta_pdf = "reporte_admin.pdf"
+    c = canvas.Canvas(ruta_pdf)
+
+    c.setFont("Helvetica-Bold", 18)
+    c.drawString(100, 750, "Informe Estadístico - Icebox Trips")
+
+    c.setFont("Helvetica", 11)
+    c.drawString(100, 725, "Análisis de metricas de usuario globales (Paradas por Ciudad).")
+    c.drawString(100, 710, "Acceso reestringido - Solo administradores.")
+
+    c.setLineWidth(1)
+    c.line(100, 690, 500, 690)
+
+    if os.path.exists(ruta_grafico):
+        c.drawImage(ruta_grafico, 100, 350, width=400, height=300)
+
+    c.showPage()
+    c.save()
+    return ruta_pdf
