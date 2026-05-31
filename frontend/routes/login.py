6643fc165febc94fb63e26196ce4ae8ddc5cee25
Lujan_Bajinay <lbajinay@fi.uba.ar>
2026-05-30 23:46:35 -0300

    Creacion de la vista del registro de usuarios y la implementacion de sus endpoints en login.py

--- frontend/routes/login.py
+++ frontend/routes/login.py
@@ -1,6 +1,50 @@
-from flask import Blueprint, render_template, redirect, url_for, session, flash, request
+from flask import Blueprint, render_template, redirect, url_for, session, flash, request, json
+import urllib.request as cliente_http
+from urllib.error import URLError, HTTPError
 
 login_bp = Blueprint('login', __name__)
+
+@login_bp.route('/registro', methods=['GET'])
+def vista_registro():
+    return render_template('registro.html')
+
+@login_bp.route('/', methods=['POST'])
+def procesar_registro():
+    nombre = request.form.get('nombre_usuario')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    datos_registro = {
+        "nombre_usuario": nombre,
+        "email": email,
+        "password": password
+    }
+
+    url_backend = "http://127.0.0.1:5000/endpoints/usuarios"
+
+    try:
+        data_json = json.dumps(datos_registro).encode('utf-8')
+
+        req = cliente_http.Request(
+            url_backend,
+            data=data_json,
+            headers={'Content-Type': 'application/json'},
+            method='POST'
+        )
+
+        with cliente_http.urlopen(req) as respuesta:
+            if respuesta.status == 201 or respuesta.status == 200:
+                flash("¡Cuenta creada con exito. Ya podes iniciar sesion", "success")
+                return redirect(url_for('login.login'))
+        
+    except HTTPError as e:
+        flash("Error al registrarse. Revisá los datos.", "error")
+        return redirect(url_for('login.vista_registro'))
+    except URLError:
+        flash("No se pudo conectar con el servidor.", "error")
+        return redirect(url_for('login.vista_registro'))
+    
+    return redirect(url_for('login.vista_registro'))
 
 @login_bp.route('/login', methods=['GET', 'POST'])
 def login():
