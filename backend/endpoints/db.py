d08a283ddbecac265f74a6cff77402c6aaa05893
agus-enc <aencalada@fi.uba.ar>
2026-06-12 01:01:00 -0300

    Modifico las conexiones a la BD para que tomen variables del sistema

--- backend/endpoints/db.py
+++ backend/endpoints/db.py
@@ -6,9 +6,9 @@ def get_connection():
     return connect(
         host=os.getenv('DB_HOST', 'localhost'),
         port=int(os.getenv('DB_PORT', 3306)),
-        user="root",
-        password="root",
-        database="ICEBOX"
+        user=os.getenv('DB_USER', 'root'),
+        password=os.getenv('DB_PASSWORD', 'root'),
+        database=os.getenv('DB_NAME', 'ICEBOX')
     )
 
 def ejecutar_consulta(sql: str, parametros: dict) -> list:
--- backend/seeder.py
+++ backend/seeder.py
@@ -6,9 +6,9 @@ import os
 DB_CONFIG = {
     'host': os.getenv('DB_HOST', 'localhost'),
     'port': int(os.getenv('DB_PORT', 3306)),
-    'user': 'root',
-    'password': 'root',
-    'database': 'ICEBOX',
+    'user': os.getenv('DB_USER', 'root'),
+    'password': os.getenv('DB_PASSWORD', 'root'),
+    'database': os.getenv('DB_NAME', 'ICEBOX'),
     'charset': 'utf8mb4'
 }
 
